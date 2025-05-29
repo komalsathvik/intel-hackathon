11f043de61054ed5b3a23dc0f5afe1638bc6d101
Sathvik <mksathvik03@gmail.com>
2025-05-30 01:51:23 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin:["http://localhost:5173"],
+    origin:["intel-hackathon-two.vercel.app"],
     methods:["GET","POST","PUT","DELETE"],
     credentials:true,
 }));
